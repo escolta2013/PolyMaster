3b9d3fc4eb64768e7b726665d9f1b9c487e3880b
escolta2013 <ssi2013@gmail.com>
2026-04-02 10:23:05 -0400

    chore: remove obsolete documentation and scratchpad scripts from polymaster directory

--- backend/inspect_lib.py
+++
@@ -1,7 +0,0 @@
-import py_clob_client
-import py_clob_client.clob_types
-from py_clob_client.client import ClobClient
-
-print("Py Clob Client Version:", getattr(py_clob_client, "__version__", "Unknown"))
-print("ClobClient dir:", dir(ClobClient))
-print("py_clob_client.clob_types dir:", dir(py_clob_client.clob_types))
